c8659be0c88aa3616c8da436404938c9fb6a420f
kaaskyah <hayk12255256hayk@gmail.com>
2023-08-07 00:57:56 +0700

    convert-boolean-values-to-strings-yes-or-no

---
+++ python/8 kyu/convert-boolean-to-strings.py
@@ -0,0 +1,5 @@
+# Завершите метод, который принимает булево значение и возвращает строку
+#  "Yes" для true или строку "No" для false.
+
+def bool_to_word(boolean):
+    return ('Yes' if boolean == True else 'No')
